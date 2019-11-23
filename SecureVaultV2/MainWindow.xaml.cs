9af1aa91bbbc9bde2b1ff306def1d96445bf79d1
adarshrise <adarshrise20@gmail.com>
2019-11-23 10:06:27 +0530

    Custom Message Box, Not working

--- SecureVaultV2/MainWindow.xaml.cs
+++ SecureVaultV2/MainWindow.xaml.cs
@@ -1,10 +1,34 @@
-﻿namespace SecureVaultV2
+﻿using HandyControl.Controls;
+using HandyControl.Data;
+using HandyControl.Tools;
+using HandyControl.Tools.Extension;
+using HandyControl.Expression;
+using HandyControl.Media.Animation;
+using HandyControl.Properties.Langs;
+using HandyControl.Interactivity;
+using MessageBox = HandyControl.Controls.MessageBox;
+
+namespace SecureVaultV2
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Close_But_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Ask("This is great");
+            MessageBox.Show(new MessageBoxInfo
+            {
+                MessageBoxText = "Ask",
+                Caption = "Title",
+                Button = MessageBoxButton.YesNo, 
+                IconBrushKey = ResourceToken.AccentBrush,
+                IconKey = ResourceToken.AskGeometry,
+                Style = ResourceHelper.GetResource<Style>("MessageBoxCustom")
+            });
         }
     }
 }
