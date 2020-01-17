122de90f6089ba10645af64bd182393b2d730783
adarshrise <adarshrise20@gmail.com>
2020-01-17 18:38:11 +0530

    DB Lock Removed, Improved Security, moves to new Window

--- SecureVaultV2/UCSignIn.xaml.cs
+++ SecureVaultV2/UCSignIn.xaml.cs
@@ -44,43 +44,61 @@ namespace SecureVaultV2
                 //String query = " Record(Id int,EmailID varchar(40) primary key,Password varchar(40) not null );";
                 string query = "select Pass from Register where nameId='" + VaultIdNametxt.Text + "';";
 
-                System.Data.SQLite.SQLiteConnection sqlcon = new System.Data.SQLite.SQLiteConnection(Tools.getDbcon());
-                sqlcon.Open();
-                System.Data.SQLite.SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
 
-                try
+                using (System.Data.SQLite.SQLiteConnection sqlcon = new System.Data.SQLite.SQLiteConnection(Tools.getDbcon()))
                 {
-                    com.ExecuteNonQuery();
-                    System.Data.SQLite.SQLiteDataReader dr = com.ExecuteReader();
-                    dr.Read();
 
-                    //MessageBox.Show(dr["Password"].ToString());
 
-                    if (LoginPasstxt.Password.ToString() == dr["Pass"].ToString())
+                    sqlcon.Open();
+
+                    using (System.Data.SQLite.SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon))
                     {
-                        HandyControl.Controls.MessageBox.Success
-                        ("Welcome, You have logged In ", "Happy to see you again");
-                        // MessageBox.Show(getInfo.getLog().ToString());
-                        Tools.setLog(true);
-                        //MessageBox.Show(getInfo.getLog().ToString());
+
+
+
+                        try
+                        {
+                            com.ExecuteNonQuery();
+
+
+                            using (System.Data.SQLite.SQLiteDataReader dr = com.ExecuteReader())
+                            {
+
+
+                                dr.Read();
+
+                                //MessageBox.Show(dr["Password"].ToString());
+
+                                if (LoginPasstxt.Password.ToString() == dr["Pass"].ToString())
+                                {
+                                    HandyControl.Controls.MessageBox.Success
+                                    ("Welcome, You have logged In ", "Happy to see you again");
+                                    // MessageBox.Show(getInfo.getLog().ToString());
+                                    Tools.setLog(true);
+                                    //MessageBox.Show(getInfo.getLog().ToString());
+                                }
+                                else
+                                {
+                                    HandyControl.Controls.MessageBox.Error
+                                    (" Error in login, Is your Id and Password Correct? ", " Login Error");
+                                }
+
+                                dr.Close();
+                            }
+
+                            com.Dispose();
+                        }
+                        catch (Exception error)
+                        {
+                            HandyControl.Controls.MessageBox.Error
+                            (" Error in login, Is your Id and Password Correct? ", " Login Error");
+                            HandyControl.Controls.MessageBox.Show(error.ToString());
+                        }
+
                     }
-                    else
-                    {
-                        HandyControl.Controls.MessageBox.Error
-                        (" Error in login, Is your Id and Password Correct? ", " Login Error");
-                    }
+                    sqlcon.Close();
 
                 }
-                catch (Exception error)
-                {
-                    HandyControl.Controls.MessageBox.Error
-                    (" Error in login, Is your Id and Password Correct? ", " Login Error");
-                    HandyControl.Controls.MessageBox.Show(error.ToString());
-                }
-
-                sqlcon.Close();
-
-
             }
         }
     }
--- SecureVaultV2/UCSignUp.xaml.cs
+++ SecureVaultV2/UCSignUp.xaml.cs
@@ -58,145 +58,175 @@ namespace SecureVaultV2
                 SQLiteConnection.CreateFile(Tools.getDbPath());
             }
 
-           
-            System.Data.SQLite.SQLiteConnection sqlcon = new System.Data.SQLite.SQLiteConnection(Tools.getDbcon());
+
+            // System.Data.SQLite.SQLiteConnection sqlcon = new System.Data.SQLite.SQLiteConnection(Tools.getDbcon());
+
+            using (System.Data.SQLite.SQLiteConnection sqlcon = new System.Data.SQLite.SQLiteConnection(Tools.getDbcon()))
+            {
 
 
-            if (string.IsNullOrWhiteSpace(Idtxt.Text))
-            {
-                HandyControl.Controls.MessageBox.Warning("Error in Vault Id Field, Please Check it again.", "Registration Error");
-            }
-            else if (string.IsNullOrWhiteSpace(nametxt.Text))
-            {
-                HandyControl.Controls.MessageBox.Warning("Error in Name Field, Please Check it again.", "Registration Error");
-            }
-            else if (string.IsNullOrWhiteSpace(Passtxt.Password))
-            {
-                HandyControl.Controls.MessageBox.Warning("Error in Password Field, Please Check it again.", "Registration Error");
-            }
-            else if (string.IsNullOrWhiteSpace(RePasstxt.Password))
-            {
-                HandyControl.Controls.MessageBox.Warning("Error in Re-Pass Field, Please Check it again.", "Registration Error");
 
-            }
-            else if (!string.IsNullOrWhiteSpace(nametxt.Text) && !string.IsNullOrWhiteSpace(Idtxt.Text) && !string.IsNullOrWhiteSpace(Passtxt.Password) && !string.IsNullOrWhiteSpace(RePasstxt.Password) && RePasstxt.Password == Passtxt.Password)
-            {
-                sqlcon.Open();
-                //MessageBox.Show(sqlcon.ToString());
-                
-                string query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Register';";
-                SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
-                SQLiteCommand com2;
-                query = com.ExecuteReader().Read().ToString();
-               // HandyControl.Controls.Growl.SuccessGlobal(query); // outside window
-                if(query==false.ToString())
+                if (string.IsNullOrWhiteSpace(Idtxt.Text))
                 {
-                    //HandyControl.Controls.Growl.SuccessGlobal("It Works");
-                           query= "create table Register (nameId varchar(40) primary key,name varchar(40),Pass varchar(40));";                                              //HandyControl.Controls.Growl.SuccessGlobal(com.ExecuteReader().ToString()); // outside window
-                           com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
-                           com.ExecuteNonQuery();
-                  
-                   
+                    HandyControl.Controls.MessageBox.Warning("Error in Vault Id Field, Please Check it again.", "Registration Error");
+                }
+                else if (string.IsNullOrWhiteSpace(nametxt.Text))
+                {
+                    HandyControl.Controls.MessageBox.Warning("Error in Name Field, Please Check it again.", "Registration Error");
+                }
+                else if (string.IsNullOrWhiteSpace(Passtxt.Password))
+                {
+                    HandyControl.Controls.MessageBox.Warning("Error in Password Field, Please Check it again.", "Registration Error");
+                }
+                else if (string.IsNullOrWhiteSpace(RePasstxt.Password))
+                {
+                    HandyControl.Controls.MessageBox.Warning("Error in Re-Pass Field, Please Check it again.", "Registration Error");
 
                 }
-                
-                com.Dispose();
-                sqlcon.Close();
-                sqlcon.Open();
-
-
-                query = "select  nameId from Register where nameId='" + Idtxt.Text + "';";
-
-                com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
-
-                try
+                else if (!string.IsNullOrWhiteSpace(nametxt.Text) && !string.IsNullOrWhiteSpace(Idtxt.Text) && !string.IsNullOrWhiteSpace(Passtxt.Password) && !string.IsNullOrWhiteSpace(RePasstxt.Password) && RePasstxt.Password == Passtxt.Password)
                 {
-                    com.ExecuteNonQuery();
-                    System.Data.SQLite.SQLiteDataReader dr1 = com.ExecuteReader();
-                    dr1.Read();
+                    sqlcon.Open();
+                    //MessageBox.Show(sqlcon.ToString());
 
-                    //MessageBox.Show(dr["Password"].ToString());
-
-                    if (Idtxt.Text == dr1["nameId"].ToString())
+                    string query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Register';";
+                    using (SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon))
                     {
-                       // MessageBox.Show("Welcome, You have logged In ", "Happy to see you again", MessageBoxButton.OK, MessageBoxImage.Information);
-                        HandyControl.Controls.MessageBox.Info("Choose a new ID", "ID Taken");
-                        // MessageBox.Show(getInfo.getLog().ToString());
-                       // getInfo.setLog();
-                        //MessageBox.Show(getInfo.getLog().ToString());
-                    }
-                    dr1.Close();
-                 
-                }
-                catch (Exception error)
-                {
-                    //MessageBox.Show(" Error in login, Is your Id and Password Correct? ", " Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    // MessageBox.Show(error.ToString());
+                        // SQLiteCommand com2;
 
-                    // HandyControl.Controls.MessageBox.Fatal("Line before Error", " In Catch ");
-                    //HandyControl.Controls.MessageBox.Fatal(error.ToString() , " In Catch " );
-                    com.Dispose();
+                        using (System.Data.SQLite.SQLiteDataReader dr1 = com.ExecuteReader())
+                        {
+                           query= dr1.Read().ToString();
+                        }
+
+
+                           /// query = com.ExecuteReader().Read().ToString();
+                  
                     
+                    
+                    
+                    }
+                        // HandyControl.Controls.Growl.SuccessGlobal(query); // outside window
+                        if (query == false.ToString())
+                        {
+                            //HandyControl.Controls.Growl.SuccessGlobal("It Works");
+                            query = "create table Register (nameId varchar(40) primary key,name varchar(40),Pass varchar(40));";                                              //HandyControl.Controls.Growl.SuccessGlobal(com.ExecuteReader().ToString()); // outside window
+                            using (SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon))
+                            {
+                               // com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
+                                com.ExecuteNonQuery();
+
+                            }
+
+                        }
+                    
+                
                     sqlcon.Close();
                     sqlcon.Open();
 
-                 
 
-                    
-               query = "insert into Register values('" + nametxt.Text.ToString() + "','" + Idtxt.Text.ToString() + "','" + RePasstxt.Password.ToString() + "');";
-               com2 = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
+                    query = "select  nameId from Register where nameId='" + Idtxt.Text + "';";
+
+                   // com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
 
                     try
                     {
-                        com2.ExecuteNonQuery();
-                        HandyControl.Controls.MessageBox.Success("This Can't be changed later", "Always Remember ");
-                        HandyControl.Controls.MessageBox.Success("Welcome , You have been successefully registered ", "Registration Completed");
-                    }
+                        using (SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon))
+                        {
+                            com.ExecuteNonQuery();
+                           // System.Data.SQLite.SQLiteDataReader dr1 = com.ExecuteReader();
+                            using (System.Data.SQLite.SQLiteDataReader dr1 = com.ExecuteReader())
+                            {
 
-                    catch (Exception error2)
+
+
+                                dr1.Read();
+
+                                //MessageBox.Show(dr["Password"].ToString());
+
+                                if (Idtxt.Text == dr1["nameId"].ToString())
+                                {
+                                    // MessageBox.Show("Welcome, You have logged In ", "Happy to see you again", MessageBoxButton.OK, MessageBoxImage.Information);
+                                    HandyControl.Controls.MessageBox.Info("Choose a new ID", "ID Taken");
+                                    // MessageBox.Show(getInfo.getLog().ToString());
+                                    // getInfo.setLog();
+                                    //MessageBox.Show(getInfo.getLog().ToString());
+                                }
+                            }
+                            //dr1.Close();
+                        }
+
+                    }
+                    catch (Exception error)
                     {
-                        HandyControl.Controls.MessageBox.Info("Choose a new ID", "ID Taken");
-                        HandyControl.Controls.MessageBox.Info(error2.ToString());
+                        //MessageBox.Show(" Error in login, Is your Id and Password Correct? ", " Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        // MessageBox.Show(error.ToString());
+
+                        // HandyControl.Controls.MessageBox.Fatal("Line before Error", " In Catch ");
+                        //HandyControl.Controls.MessageBox.Fatal(error.ToString() , " In Catch " );
+                      
+
+                        sqlcon.Close();
+                        sqlcon.Open();
+
+
+
+
+                        query = "insert into Register values('" + nametxt.Text + "','" + Idtxt.Text + "','" + RePasstxt.Password + "');";
+                       // com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
+
+                        using (SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(query, sqlcon))
+                        {
+                            try
+                            {
+                                com.ExecuteNonQuery();
+                                HandyControl.Controls.MessageBox.Success("This Can't be changed later", "Always Remember ");
+                                HandyControl.Controls.MessageBox.Success("Welcome , You have been successefully registered ", "Registration Completed");
+                            }
+
+                            catch (Exception error2)
+                            {
+                                HandyControl.Controls.MessageBox.Info("Choose a new ID", "ID Taken");
+                                HandyControl.Controls.MessageBox.Info(error2.ToString());
+                            }
+                        }
+                      //  com2.Dispose();
+                       // sqlcon.Close();
+
                     }
 
-                    com2.Dispose();
+
+
+
+                    /*
+
+
+
+
+                    //query = " select nameId from Register;";
+                    query = "select count(*) from Register ;";
+                    com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
+                    com.ExecuteNonQuery();
+                    System.Data.SQLite.SQLiteDataReader dr = com.ExecuteReader();
+
+
+                    HandyControl.Controls.Growl.SuccessGlobal("It Works "+ dr.Read().ToString()); // Returns True when count>0
+
+
+
+                    */
+
+
+
                     sqlcon.Close();
 
                 }
-
-
-              
-
-                /*
-
-
-
-
-                //query = " select nameId from Register;";
-                query = "select count(*) from Register ;";
-                com = new System.Data.SQLite.SQLiteCommand(query, sqlcon);
-                com.ExecuteNonQuery();
-                System.Data.SQLite.SQLiteDataReader dr = com.ExecuteReader();
-              
-
-                HandyControl.Controls.Growl.SuccessGlobal("It Works "+ dr.Read().ToString()); // Returns True when count>0
-
-
-
-                */
-
-
-
-                sqlcon.Close();
+                else
+                {
+                    HandyControl.Controls.MessageBox.Error("Pass Code are not same, Please Check it again.", "Registration Error");
+                }
+                // MessageBox.Show(RePasstxt.Password);// this works
 
             }
-            else
-            {
-                HandyControl.Controls.MessageBox.Error("Pass Code are not same, Please Check it again.", "Registration Error");
-            }
-            // MessageBox.Show(RePasstxt.Password);// this works
-
-
         }
     }
 }
