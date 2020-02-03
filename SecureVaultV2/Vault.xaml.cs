31665056953526b9f77ebd92109615fc85d2dd33
adarshrise <adarshrise20@gmail.com>
2020-02-04 00:53:50 +0530

    Added UC to Secure Vault Display Window

--- SecureVaultV2/Vault.xaml.cs
+++ SecureVaultV2/Vault.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,14 +22,30 @@ namespace SecureVaultV2
     /// </summary>
     public partial class Vault : Window
     {
+        static UserControl usc1 = new UCTWB();
+
         public Vault()
         {
             InitializeComponent();
+            //VaultDisplayWindow.Children.Clear();
             ShowMe();
+           // Thread.Sleep(2000);
+            VaultDisplayWindow.Children.Add(usc1);
+           
+           
+
+            this.Closed += new EventHandler(MainWindow_Closed);
+
         }
         public void ShowMe()
         {
             (FindResource("showMe") as Storyboard).Begin(this);
+        }
+
+        void MainWindow_Closed(object sender, EventArgs e)
+        {
+            VaultDisplayWindow.Children.Clear();
+           // VaultDisplayWindow.Children.Remove(usc1);
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
